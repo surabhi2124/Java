9d80069c4a42645ab0c6443de487c746fb27b65a
surabhi2124 <surabhisripadrao21@gmail.com>
2025-03-03 15:30:57 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Geeks {
+public class HelloWorld {
   
     public static void main(String[] args)
         throws IOException
